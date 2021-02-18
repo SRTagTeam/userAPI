5ac6ea4be9c3abd02158fef95db0de0dc9001e63
Rajeswarakumar <Rajeswarakumar@gmail.com>
2021-02-18 12:01:16 +0530

    Trigger deployment from VS Code commit

--- app/routes/v1/healthCheck.js
+++ app/routes/v1/healthCheck.js
@@ -2,7 +2,7 @@ var express = require('express');
 router = express.Router();
 
 router.get('/', function(req, res){
-    res.send('API is working in Dev!!!');
+    res.send('API is working in Dev from VS Code!');
   });
 
 module.exports = router;
